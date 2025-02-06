d802ae0ebc091b2fbbaacfd74250ac2a08c0939f
Hardik19102003 <202103032@daiict.ac.in>
2025-02-07 01:57:56 +0530

    Define the Object Model

---
+++ models/object.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+// Object represents the database table structure
+type Object struct {
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+}
